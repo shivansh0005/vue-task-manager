c8892565e72194ec929c73e5317d39eedc522793
Your Name <shivansh chaturvedi>
2025-12-04 02:16:15 +0530

    feat: implement TaskItem component with toggle & delete

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,14 @@
   <main class="app">
     <h1>Vue Task Manager</h1>
 <AddTask @add-task="handleAddTask"/>
+
+ <TaskItem
+      v-for="t in tasks"
+      :key="t.id"
+      :task="t"
+      @toggle-task="toggleTask(t.id)"
+      @delete-task="deleteTask(t.id)"
+    />
     <div id="app-root">
       
     </div>
@@ -11,9 +19,25 @@
 <script setup>
 
 import AddTask from './components/AddTask.vue';
+import TaskItem from './components/TaskItem.vue';
+import {ref} from 'vue';
 
+const tasks=ref([]);
 function handleAddTask(text){
-  console.log(" Task Added:",text);
+   tasks.value.push({
+    id: Date.now(),
+    text,
+    completed: false
+  });
+}
+
+function toggleTask(id) {
+  const t = tasks.value.find(t => t.id === id);
+  if (t) t.completed = !t.completed;
+}
+
+function deleteTask(id) {
+  tasks.value = tasks.value.filter(t => t.id !== id);
 }
 </script>
 
---
+++ src/components/TaskItem.vue
@@ -0,0 +1,70 @@
+<script setup>
+
+
+//receive task from parent
+const props=defineProps({
+    task:{
+        type:String,
+        required:true
+    }
+});
+
+const emit=defineEmits(["toggle-task","delete-task"]);
+
+
+</script>
+
+<template>
+    <div class="task-item"
+    :class="{completed:task.completed}"
+    >
+    <div class="left">
+
+    <input
+    type="checkbox"
+    :checked="task.completed"
+    @change="emit('toggle-task')"
+    />
+    <span>{{task.text}}</span>
+
+    </div>
+
+    <button @click="emit('delete-task')">x</button>
+    
+    
+    
+    </div>
+</template>
+
+<style scoped>
+.task-item {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background: #222;
+  padding: 12px;
+  margin-bottom: 12px;
+  border-radius: 6px;
+}
+
+.left {
+  display: flex;
+  align-items: center;
+  gap: 10px;
+}
+
+.completed span {
+  text-decoration: line-through;
+  opacity: 0.6;
+}
+
+button {
+  background: red;
+  color: white;
+  border: none;
+  padding: 6px 10px;
+  border-radius: 4px;
+  cursor: pointer;
+}
+</style>
+
